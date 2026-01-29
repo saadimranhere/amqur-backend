8d847742384f73a30d934a2aa5cf8a2e09f8eb3f
Saad Imran <Saad@Hurias-MacBook-Pro.local>
2026-01-29 15:02:37 -0600

    temporary public register for bootstrap admin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private auth: AuthService) { }
 
     @Post('register')
+    @Public()
     register(@Body() dto: RegisterDto) {
         return this.auth.register(dto);
     }
