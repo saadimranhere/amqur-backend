bb5910f4f6ace3a1795556ef5a7e3ae2f7695bd5
Saad Imran <Saad@Hurias-MacBook-Pro.local>
2026-01-30 22:32:28 -0600

    remove auth logic from exception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -7,31 +7,20 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Catch()
+@Catch(HttpException)
 export class GlobalExceptionFilter implements ExceptionFilter {
-    catch(exception: unknown, host: ArgumentsHost) {
+    catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
 
-        let status = HttpStatus.INTERNAL_SERVER_ERROR;
-        let message = 'Internal server error';
-
-        if (exception instanceof HttpException) {
-            status = exception.getStatus();
-
-            const res = exception.getResponse();
-            message =
-                typeof res === 'string'
-                    ? res
-                    : (res as any).message ?? exception.message;
-        }
+        const status = exception.getStatus();
 
         response.status(status).json({
             success: false,
             status,
             path: request.url,
-            message,
+            message: exception.message,
             timestamp: new Date().toISOString(),
         });
     }
